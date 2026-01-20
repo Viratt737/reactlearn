132afccec1a7775d5ee998c374ea904f74657c53
virat <viratt737@gmail.com>
2026-01-20 19:56:31 +0530

    create second folder learn about class based component using vite

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,4 +1,3 @@
-
 function App() {
     <div>
       <h1>hello from react using vite</h1>
---
+++ 02-folder-react-concepts/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ClassBasedComponent from "./component/class-based-component"
+function App() {
+
+  return (
+      <div>
+        <h1>React js concepts</h1>
+        <ClassBasedComponent/>
+      </div>
+  )
+}
+
+export default App;
