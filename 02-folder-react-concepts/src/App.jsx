8d28d9adb55ea4d9017d677e0e8511ee8a8ba1c9
virat <viratt737@gmail.com>
2026-01-20 20:09:16 +0530

    learn about functional component in vite

--- 02-folder-react-concepts/src/App.jsx
+++ 02-folder-react-concepts/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import ClassBasedComponent from "./component/class-based-component"
+import FunctionalComponent from "./component/functional-based-component"
 function App() {
 
   return (
       <div>
         <h1>React js concepts</h1>
         <ClassBasedComponent/>
+        <FunctionalComponent/>
       </div>
   )
 }
---
+++ 02-folder-react-concepts/src/component/functional-based-component.jsx
@@ -0,0 +1,11 @@
+
+
+function FunctionalComponent (){
+    return (
+    <div>
+        <h3>functional component in react</h3>
+    </div>
+    );
+}
+
+export default FunctionalComponent;
