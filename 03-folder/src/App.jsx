cda346e2bf50abaee6a0f83e15100836c38e41a1
virat <viratt737@gmail.com>
2026-02-09 14:57:58 +0530

    learn about usee effect, how we use use effect

---
+++ 03-folder/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let counter = 5
+  const addValue = () =>{
+    console.log("value added", Math.random());
+  }
+  return (
+    <>
+      <h1>chai aur react</h1>
+      <h2>counter value : {counter}</h2>
+      <button
+       onClick={addValue}
+      >Add value</button>
+      <br />
+      <button> remove value</button>
+    </>
+  )
+}
+
+export default App
