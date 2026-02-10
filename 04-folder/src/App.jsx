36a4e9304803cccafbf0a9f0a01a61a00e513f59
virat <viratt737@gmail.com>
2026-02-11 00:54:11 +0530

    learn more about props and components and tailwind css in react

--- 04-folder/src/App.jsx
+++ 04-folder/src/App.jsx
@@ -2,28 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
-
-     <div class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
-  <div>
-    <img class="size-48 shadow-xl rounded-md" alt="" src="https://i.pinimg.com/736x/e8/30/01/e83001d7a214a0037c115c5fa9961ad2.jpg" />
-  </div>
-  <div class="flex items-center md:items-start">
-    <span class="text-2xl font-medium">Class Warfare</span>
-    <span class="font-medium text-sky-500">The Anti-Patterns</span>
-    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
-      <span>No. 4</span>
-      <span>·</span>
-      <span>2025</span>
-    </span>
-  </div>
-</div>
+     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-5'>Tailwind Test</h1>
+      <Card username = "virat trivedi"/>
+      <Card username = "hahahahah"/>
     </>
   )
 }
