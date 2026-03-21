6935cc88354898777a2bb9d28bc238ce6380651c
virat <viratt737@gmail.com>
2026-03-21 10:54:08 +0530

    lern some basic

--- 06-folder/src/App.jsx
+++ 06-folder/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   
+  const [user, setUser] = useState(10);
+  function abc(){
+    setUser(30)
+  }
 
   return (
     <>
+      {/* <div>
+         <h1>hello</h1>
+         <button onClick={abc}>click me</button>
+      </div> */}
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>username is {user}</h1>
+        <button onClick={abc}>click me to change user</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
